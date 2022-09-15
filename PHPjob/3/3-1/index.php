b974450bff039934e780ccb5aa17ea0d37def445
2ruta <107760588+2ruta@users.noreply.github.com>
2022-09-15 15:31:31 +0900

    ファイル変えた

--- PHPjob/3/3-1/index.php
+++ PHPjob/3/3-1/index.php
@@ -1,5 +1,18 @@
 <?php
-
-echo "Hello World!!";
-
+// PHPの研修カリキュラム2-1
 ?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+1-1
+*/
+?>
+<html>
+    <?php
+    // index.php
+    $message = "Hello World" ;
+    echo $message ; 
+    ?>
+</html>
