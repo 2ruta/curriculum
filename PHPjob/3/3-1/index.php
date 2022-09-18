b79eb8be8ba203b1a5a07e7fb07f4eb9f2c2555e
2ruta <107760588+2ruta@users.noreply.github.com>
2022-09-18 15:49:54 +0900

    指摘箇所修正しました。2022/09/18

--- PHPjob/3/3-1/index.php
+++ PHPjob/3/3-1/index.php
@@ -5,9 +5,9 @@
     $name="taro";
     if($pass="pass"&&$name="taro"){
         echo "ログイン成功です";
-    } else if($pass="pass"&&$name!=="taro"){
+    } else if($pass="pass"){
         echo "名前が違っています";
-    } else if($pass!=="pass"&&$name==="taro"){
+    } else if($name="taro"){
         echo "パスワードが違っています";
     } else{
         echo "入力情報が間違っています ";
