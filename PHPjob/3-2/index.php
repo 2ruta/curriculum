49fbc8b5a3007ac2d6e52cd1d8b41d4b40ad4f67
2ruta <107760588+2ruta@users.noreply.github.com>
2022-09-24 19:22:47 +0900

    直しました

--- PHPjob/3-2/index.php
+++ PHPjob/3-2/index.php
@@ -4,21 +4,34 @@
     //フルーツとその単価を対応させた連想配列を定義してください
     $fruits = ["りんご"=>100,"みかん"=>150,"桃"=>500];
     //それぞれ配列の0:リンゴ、1:みかん、2:桃　の順に個数を入力した配列を作成してください
-    $quantity = [3,1,6];
+    $quantity = ["りんご"=>3,"みかん"=>1,"桃"=>6];
 
     //step2
     //それぞれのフルーツの合計金額を計算する関数を定義してください。
     //引数はフルーツの単価・個数の２つ、返り値は計算した合計値を返します。
-    function TotalPrice($fruitsas $key=>$value,$quantity){
-        $price=$value*$quantity;
-        print $fruits."の値段は".$price."だよ";
+    
+    function calculatePrice(int $fruits, int $quantity) {
+        return $fruits*$quantity;
     }
+
+
+
+    echo calculatePrice($fruits["りんご"],$quantity["りんご"]);
+    
+    echo "<br>";
+
+
+   foreach($fruits as $key => $value) {
+   echo calculatePrice($fruits[$key],$quantity[$key]);
+   echo "<br>";
+
+}
 
     //step3
     //繰り返しを使ってそれぞれのフルーツを書き出してください。
 
-    foreach($fruits as $value){
-
-        echo 
-    }
+   foreach($fruits as $key => $value){
+    echo $key;
+   echo "<br>";
+   }
 ?>
