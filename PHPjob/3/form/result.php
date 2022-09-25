c9772514df0eaba54af6bf1a1dbc96f5de16f0b7
2ruta <107760588+2ruta@users.noreply.github.com>
2022-09-25 18:32:50 +0900

    修正中

--- PHPjob/3/form/result.php
+++ PHPjob/3/form/result.php
@@ -1,6 +1,11 @@
 <?php
-$my_name = $_GET['my_name'];
-$password = $_GET['password'];
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+$my_email = $_POST['my_email'];
+echo var_dump($_POST);
 ?>
 <p>私の名前は、<?php echo $my_name; ?></p>
 <p>私のパスワードは、<?php echo $password; ?></p>
+<p>私のemailは、<?php echo $my_email; ?></p>
+
+
