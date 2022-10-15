3ccaa8a3d74d7b518b27ebe124e0660ab5a70921
2ruta <107760588+2ruta@users.noreply.github.com>
2022-10-15 19:00:59 +0900

    疑似完成

--- PHPjob/3-4/answer.php
+++ PHPjob/3-4/answer.php
@@ -1,15 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="stylesheet.css">
+ 
+  <title>3章チェックテスト</title>
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+
+$q1 = $_POST['q1'];
+$q2 = $_POST['q2'];
+$q3 = $_POST['q3'];
+$q4 = $_POST['q4'];
+$my_name =$_POST['my_name'];
 
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
 
 ?>
-<p><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
-<p>①の答え</p>
+<p><!--POST通信で送られてきた名前を表示--><?php echo $my_name; ?>さんの結果は・・・？</p>
+<p>①の答え<
+    <?php
+        if($q1 == '80'){
+	    echo '正解';
+        }else{
+	    echo '不正解';
+        }
+    ?>
+</p>
 <!--作成した関数を呼び出して結果を表示-->
 
-<p>②の答え</p>
+    <p><!--作成した関数を呼び出して結果を表示-->③の答え
+        <?php
+            if($q2 == 'HTML'){
+	        echo '正解';
+            }else{
+	        echo '不正解';
+            }
+        ?>
+    </p>
 <!--作成した関数を呼び出して結果を表示-->
 
-<p>③の答え</p>
-<!--作成した関数を呼び出して結果を表示-->
+    <p><!--作成した関数を呼び出して結果を表示-->③の答え
+        <?php
+            if($q3 == 'select'){
+	        echo '正解';
+            }else{
+	        echo '不正解';
+            }
+        ?>
+    </p>
+</html>
--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,4 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="stylesheet.css">
+ 
+  <title>3章チェックテスト</title>
+</head>
 
 <?php
 $my_name = $_POST['my_name'];
@@ -13,7 +22,7 @@ $my_name = $_POST['my_name'];
 
 <p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $my_name; ?>さん</p>
 <!--フォームの作成 通信はPOST通信で-->
-
+<br>
 <form action="question.php" method="post">
 <h2>①ネットワークのポート番号は何番？</h2>
 
@@ -22,21 +31,26 @@ $my_name = $_POST['my_name'];
 <input type="radio" name="q1" value="22"> 22
 <input type="radio" name="q1" value="20"> 20
 <input type="radio" name="q1" value="21"> 21
-
+<br>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-
+<br>
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-PHP
-Python
-JAVA
-HTML
+<input type="radio" name="q2" value="PHP"> PHP
+<input type="radio" name="q2" value="Python"> Python
+<input type="radio" name="q2" value="JAVA"> JAVA
+<input type="radio" name="q2" value="HTML"> HTML
+<br>
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-join
-select 
-insert 
-update
-
+<input type="radio" name="q3" value="join">join
+<input type="radio" name="q3" value="select">select
+<input type="radio" name="q3" value="insert">insert
+<input type="radio" name="q3" value="update">update
 <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+<br>
+<input type="submit" value="回答する" />
+
 </form>
+
+</html>
